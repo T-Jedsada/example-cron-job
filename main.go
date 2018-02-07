a4b9bd4011b730ab12cda353fe87ce2ab2d033e8
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-02-07 19:02:52 +0700

    create corn job every 15 minute

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/jasonlvhit/gocron"
+)
+
+var counter = 0
 
 func main() {
-	fmt.Println("Hello cron job golang")
+	gocron.Every(15).Minute().Do(taskWithParams)
+	startJob()
+}
+
+func startJob() {
+	<-gocron.Start()
+}
+
+func stopJob() {
+	gocron.Remove(taskWithParams)
+	gocron.Clear()
+	os.Exit(1)
+}
+
+func taskWithParams() {
+	counter++
+	if counter > 30 {
+		stopJob()
+	} else {
+		// TODO: send email to pondthaitay@hotmail.com
+	}
 }
